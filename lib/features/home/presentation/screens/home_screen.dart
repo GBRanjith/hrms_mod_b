fd126eb344641f82a4ae98cc4de3497ef5f4b3a5
GBRanjith <boopathyranjith0@gmail.com>
2026-08-18 17:53:27 +0530

    feat: add home shell with three tab screens

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../claims/presentation/screens/claim_list_screen.dart';
+import '../../../dashboard/presentation/screens/dashboard_screen.dart';
+import '../../../employee/presentation/screens/employee_list_screen.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -8,8 +12,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _index = 0;
+
+  static const List<Widget> _tabs = [
+    DashboardScreen(),
+    EmployeeListScreen(),
+    ClaimListScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: IndexedStack(index: _index, children: _tabs),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _index,
+        onDestinationSelected: (value) => setState(() => _index = value),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.dashboard_outlined),
+            selectedIcon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.groups_outlined),
+            selectedIcon: Icon(Icons.groups),
+            label: 'Directory',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.receipt_long_outlined),
+            selectedIcon: Icon(Icons.receipt_long),
+            label: 'Claims',
+          ),
+        ],
+      ),
+    );
   }
 }
